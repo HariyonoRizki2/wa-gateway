f11f08a185efea45b1273bc4c233846013dee855
HARIYONO RIZKI <nabilaputridwianjani526@gmail.com>
2022-12-16 18:23:35 +0700

    Update koneksi.php

--- helper/koneksi.php
+++ helper/koneksi.php
@@ -7,5 +7,5 @@ $db = "railway";
 error_reporting(0);
 $koneksi = mysqli_connect($host, $username, $password, $db) or die("GAGAL");
 
-$base_url = "http://localhost/wabot/";
+$base_url = "https://wa-gateway-production.up.railway.app/";
 date_default_timezone_set('Asia/Jakarta');
